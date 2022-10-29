e2617cf1cafd4542471c26651086ede4c9299a78
precious-c <preciousamaeechi111@yahoo.com>
2022-10-29 11:14:28 +0000

    Basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python3
 
 import uuid
-import datetime
+from datetime import datetime
 
 class BaseModel:
     """Base model of class """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initializee instance attributes id, created_at, and updated_at"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key == "__class__":
+                    continue
+                setattr(self, key, value)
+
+        else:        
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns class attributes in string format"""
@@ -17,7 +26,7 @@ class BaseModel:
 
     def save(self):
         """Updates the updated_at attribute with current time"""
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """Returns a dictionary representation of class attributes"""
@@ -26,5 +35,4 @@ class BaseModel:
         new_dict["created_at"] = self.created_at.isoformat()
         new_dict["updated_at"] = self.updated_at.isoformat()
         return new_dict
-
 
